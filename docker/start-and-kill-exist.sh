070a71c2a6057755ed6ed297a372144a81b074da
pma @ ikga <pma@rdorte.org>
2017-04-13 18:20:53 +0200

    building docker image in more steps

---
+++ docker/start-and-kill-exist.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Start up exist, and then shutdown: useful to prepare collection in
+# docker image (indexes on creation of image, not afterwards).
+
+java -jar start.jar jetty &
+EXPROC=$!
+echo "Waiting for completion of existdb ..."
+sleep 5
+grep -m 1 "Server has started, listening on" <(tail -f ./webapp/WEB-INF/logs/exist.log)
+echo "Shut down existdb ($EXPROC) ..."
+kill -SIGTERM $EXPROC
