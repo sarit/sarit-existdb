2ecbb34363d77422637cae7233b81c5ba6c578f4
Patrick McAllister <pma@rdorte.org>
2017-04-15 15:42:30 +0200

    make sure log is there

--- docker/start-and-kill-exist.sh
+++ docker/start-and-kill-exist.sh
@@ -3,10 +3,11 @@
 # Start up exist, and then shutdown: useful to prepare collection in
 # docker image (indexes on creation of image, not afterwards).
 
+mkdir -p ./webapp/WEB-INF/logs/
+touch ./webapp/WEB-INF/logs/exist.log
 java -jar start.jar jetty &
 EXPROC=$!
 echo "Waiting for completion of existdb ..."
-sleep 5
 grep -m 1 "Server has started, listening on" <(tail -f ./webapp/WEB-INF/logs/exist.log)
 sleep 10
 echo "Shut down existdb ($EXPROC) ..."
