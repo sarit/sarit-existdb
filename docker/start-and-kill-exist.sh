d3e63fbec53fb1a659ef54cdd6fd3414f6ea1f1f
Patrick McAllister <pma@rdorte.org>
2017-04-15 14:19:34 +0200

    clean up, don't force exist into shutdown immediately

--- docker/start-and-kill-exist.sh
+++ docker/start-and-kill-exist.sh
@@ -8,5 +8,6 @@ EXPROC=$!
 echo "Waiting for completion of existdb ..."
 sleep 5
 grep -m 1 "Server has started, listening on" <(tail -f ./webapp/WEB-INF/logs/exist.log)
+sleep 10
 echo "Shut down existdb ($EXPROC) ..."
 kill -SIGTERM $EXPROC
